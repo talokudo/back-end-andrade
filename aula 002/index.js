709cb037e3f252da608a90f0cd9acb86c7f17142
talokudo <pipocacomsal0102@gmail.com>
2025-03-18 10:33:16 -0300

    g

--- aula 002/index.js
+++ aula 002/index.js
@@ -5,36 +5,31 @@ const { error } = require('console');
 const app = express();
 app.use(express.json());
 
-app.post("./users",(req, res)=>{
-    const {nome, email} = req.body;
-    if(!nome || !email){
-        return res.status(400).json({error: "nome e email sãp obrigatorios!"})
+app.post("/users", (req, res) => {
+    const { nome, email } = req.body;
+    if (!nome || !email) {
+        return res.status(400).json({ error: "nome e email são obrigatórios!" });
     }
 
     const user = userService.addUser(nome, email);
-    res.status(200).json({user});
-
+    res.status(200).json({ user });
 });
 
 app.get("/users", (req, res) => {
     res.json(userService.getUsers());
 });
 
+app.delete("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const resultado = userService.deleteUser(id);
+        res.status(200).json({ resultado });
+    } catch (erro) {
+        res.status(404).json({ error: erro.message });
+    }
+});
+
 const port = 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("ta rodando na porta :)", port);
-})
-
-app.delete("/userers/:id", (req, res) => {
-    const id =pareseInt(req.params.id);
-} try {
-    const resultado = userService.deleteUser(id);
-}catch (erro) {
-    res.status(404).json({error: erro.message});
-
-})
-
-const port = 3000;
-app.listen(port,()=>{
-    console.log("ta rodando", port);
 });
